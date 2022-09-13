adcac023f39c7a8c7e7dfec72bd6dcde132191a5
abdelc0 <90260021+abdelc0@users.noreply.github.com>
2022-09-13 15:46:46 +0200

    feat:ik heb inlogregistratie gemaakt ,de gebruiker kan nu een acount makken en inloggen  ook kan de gebruiker na de inloggen gelijk zich aanmelden voor het opendag.

---
+++ connect_db.php
@@ -0,0 +1,14 @@
+
+<?php
+define ("SERVERNAME","localhost");
+define("USERNAME","root");
+define("PASSWORD","");
+define("DBName","opendag");
+// define ("SERVERNAME","localhost");
+// define("USERNAME","imran_m1");
+// define("PASSWORD","vxBMzpzH80TgN9rQ");
+// define("DBName","anime_da");
+
+$conn = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBName);
+
+?>  
