395e19a94e144b8e9cf61e87ea49719da5395e16
KaganAkdeniz <332960@student.mboutrecht.nl>
2022-09-16 14:16:02 +0200

    Informatie over de school en map van de locatie

---
+++ navbar/navbar.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="navbar/navbar.css">
+
+<nav class="navbar">
+        <div class="brand-title">MBO Utrecht</div>
+        <a href="#" class="toggle-button">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
+        <div class="navbar-links">
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="informatie.html">infomatie</a></li>
+            <li><a href="http://www.utrechtpizza.com/" target="blank">referenties</a></li>
+            <li><a href="contact.html">contact</a></li>
+            <li><a href="faq.html">FAQ</a></li>
+          </ul>
+        </div>
+      </nav>
