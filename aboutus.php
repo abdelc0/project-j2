38750a862207405e3ae293947f14a85d4c745bd7
abdelc0 <90260021+abdelc0@users.noreply.github.com>
2022-09-16 18:56:08 +0200

    feat:ik heb text toegevoed en de kleur vernaderd en verzorgd dan het responsive is

--- aboutus.php
+++ aboutus.php
@@ -30,20 +30,88 @@
 		<div class="article-meta">
 			
 			<p class="byline">  </p>
-			<p class="dateline"> ahwulia</p>
+			<p class="dateline"> </p>
 			<p class="article-tags">
-				
+		
 			</p>
 		</div>
+        
 	</div>
      </header>
 
-
+        
 <section>
-<figure><img src="" alt="a smiling person in a pink hoodie, standing in front of a bright pink lighted arcade basketball game. " />
+    <h2>MBO Utrecht wil jou helpen in de ontwikkeling van student tot zelfbewuste professional. Wij geven jou meer dan een opleiding en leren je meer dan een vak. Er is veel aandacht voor de ontwikkeling van talent, arbeidsvaardigheden, ondernemend gedrag, maatschappelijke betrokkenheid, respect voor elkaar en burgerschap. </h2>
+    <br>
+    <hr>
+
+<h3>1. Kleinschalige school & betrokken docenten</h3>
+<p>Bij MBO Utrecht kun je op goed bereikbare locaties wel 50 verschillende opleidingen volgen. Wij zijn een kleinschalige school waar jij je snel thuis zult voelen. Dat komt door onze betrokken docenten die niet alleen aandacht hebben voor jou als persoon, maar ook voor de manier waarop jij leert.</p>
+<figure><img src="https://www.mboutrecht.nl/wp-content/uploads/2018/09/ondertekening-akkoord-pcou-en-mbo-utrecht-1024x682.jpg" alt="a smiling person in a pink hoodie, standing in front of a bright pink lighted arcade basketball game. " />
 		<figcaption>Photo by imran (futch)</figcaption>
 	</figure>
-	<p>ahshaibibssssssssssssssssssdab abdaib ahdb ajbdilbll</p>
+	
+	
+
+	
+
+	<!-- full size image: https://images.unsplash.com/photo-1601637155580-ac6c49428450-->
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+    
+    <hr>
+
+<h3>2. Ruimte voor maatwerk</h3>
+<p>Er is veel ruimte voor maatwerk. We hebben bijvoorbeeld veel doorstroom- en versnellingsmogelijkheden. Maar er is ook ondersteuning wanneer dat nodig is.</p>
+<figure><img src="https://www.opendag-mboutrecht.nl/sites/default/files/styles/card/public/cdn/mbo-utrecht-2020-algemeen-05.jpg?itok=iTXteRn9" alt="a smiling person in a pink hoodie, standing in front of a bright pink lighted arcade basketball game. " />
+		<figcaption>Photo by imran (futch)</figcaption>
+	</figure>
+
+    <br>
+<br>
+<br>
+<br>
+<hr>
+    <h3>3. Straks een goed opgeleide professional</h3>
+<p>Bij ons word je niet alleen opgeleid voor een vak. We vinden het belangrijk dat je naast je opleiding ook vaardigheden leert die je helpen in deze wereld die continu verandert. Zo ben jij straks een goed opgeleide professional die klaar is om in de praktijk aan de slag te gaan.</p>
+<figure><img src="https://www.lentiz.nl/assets/uploads/2019/07/Beeld-bij-persbericht-_diploma.jpg" alt="a smiling person in a pink hoodie, standing in front of a bright pink lighted arcade basketball game. " />
+		<figcaption>Photo by imran (futch)</figcaption>
+	</figure>
+    <br>
+<br>
+<br>
+<br>
+<hr>
+    <h3>Over MBO Utrecht</h3>
+<p>MBO Utrecht is een kleinschalige, interconfessionele onderwijsinstelling die doelmatig onderwijs aanbiedt met focus op de hoogste niveaus van het mbo. We bieden met ca. 500 medewerkers ruim 50 mbo-opleidingen aan ongeveer 5.000 studenten, in uiteenlopende de domeinen van het mbo-onderwijs.</p>
+<figure><img src="https://www.opendag-mboutrecht.nl/sites/all/themes/mbo/ui/icon/brand/brand.jpg" alt="a smiling person in a pink hoodie, standing in front of a bright pink lighted arcade basketball game. " />
+		<figcaption>Photo by imran (futch)</figcaption>
+	</figure>
+
+
+
+	
+	
+
+	
+
+	<!-- full size image: https://images.unsplash.com/photo-1601637155580-ac6c49428450-->
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+    
+  
 	
 
 	
