f9e01d194e49aecba4b244d01c79e996da00d2fa
abdelc0 <90260021+abdelc0@users.noreply.github.com>
2022-09-16 18:09:39 +0200

    feat: ik heb de pagina netje gemaakt en de cc klopend gemaakt ik moet nu nog alleen de text en paar errors fixen

---
+++ aboutus.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>son</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="readmore.css">
+</head>
+<body>
+
+    <!-- <header>
+    <div class="row">
+                <div class="col-12 px-0">
+                    <?php include("./navbar.php"); ?>
+                </div>
+            </div>
+    </header> -->
+    <script src="script.js" type="text/javascript"></script>
+     <!-- header -->
+     <header>
+
+
+
+	
+	<div class="info-wrapper">
+		<h1 class="headline">About MBO Utrecht</h1>
+		<h2 class="subhead">ben je nieuwe op dit school of zit je nog te twijfelen hier onder zie je meer infomatie</h2>
+		<div class="article-meta">
+			
+			<p class="byline">  </p>
+			<p class="dateline"> ahwulia</p>
+			<p class="article-tags">
+				
+			</p>
+		</div>
+	</div>
+     </header>
+
+
+<section>
+<figure><img src="" alt="a smiling person in a pink hoodie, standing in front of a bright pink lighted arcade basketball game. " />
+		<figcaption>Photo by imran (futch)</figcaption>
+	</figure>
+	<p>ahshaibibssssssssssssssssssdab abdaib ahdb ajbdilbll</p>
+	
+
+	
+
+	<!-- full size image: https://images.unsplash.com/photo-1601637155580-ac6c49428450-->
+
+</section>
+<style>
+    </style>
+
+
+     
+        
+
+            <!-- nav bar -->
+
+           
+
+  
+
+
+      
+</body>
+</html>
