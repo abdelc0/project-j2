2f1974d42ae5df65bf9bf293787a145667fd6946
abdelc0 <90260021+abdelc0@users.noreply.github.com>
2022-09-16 14:42:12 +0200

    feat: ik verzorgt dat de map in een colum word gestopt

--- navigatie.php
+++ navigatie.php
@@ -4,7 +4,7 @@
 include 'navbar/navbar.php';
 ?>
 
-<div class= 'loction-info-color'>
+<!-- <div class= 'loction-info-color'>
 <div class="location-info">
     <div class="anchor" data-anchor="locatie" id="locatie">
         <span>Locatie</span>
@@ -38,5 +38,40 @@ include 'navbar/navbar.php';
         allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> </div>
 
 
-</div>
+</div> -->
+
+
+<div class="row">
+      <div class="column" style="background-color:#3c4053">
+        <div class="container">
+          <div class="yt-holder">
+          <div class="maps">
+    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9806.
+    358403289614!2d5.151535498693468!3d52.08720037733886!2m3!1f0!2f0!3f0!3m2
+    !1i1024!2i768!4f13.1!3m3!1m2!1s0x47c668a291ed7f45%3A0x9b18dabb8687ad2!2
+    sMBO%20Utrecht!5e0!3m2!1snl!2snl!4v1663069808762!5m2!1snl!2snl" width="900" height="500" style="border:0;"
+        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> </div>
+
+
+
+          
+        
+          </div>
+           <!-- <h1>560315</h1>  -->
+      </div>
+      <div class="column" style="background-color:#3c4053">
+        <h2>welkom op utrecht pizza</h2>
+        <h5>pizza wordt gemaakt van vers deeg met de beste ingrediënten.<br> We bezorgen jouw pizza gemiddeld binnen 21 minuten bij je thuis</h5>
+        <hr>
+        <div class="logotext"><img src="img/Schermafbeelding 2021-11-25 193356.png" alt=""></div>
+
+
+      </div>
+        
+        
+        
+        </div>
+    
+      </div>
+    </div>
 
