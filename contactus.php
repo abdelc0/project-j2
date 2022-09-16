0a9b78c156136490fddbd111f638e85c37b7fa89
abdelc0 <90260021+abdelc0@users.noreply.github.com>
2022-09-16 19:26:49 +0200

    feat:ik heb de contact pagina gemaakt

---
+++ contactus.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="contact.css" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+
+   
+
+    <div class="container">
+      <span class="big-circle"></span>
+      <img src="img/shape.png" class="square" alt="" />
+      <div class="form">
+        <div class="contact-info">
+          <h3 class="title">Laten we in contact komen</h3>
+          <p class="text">
+            Heb je een vraag of opmerking? Neem contact met ons op via onze sociale kanalen! Wist je dat we elke dag van 8.00 tot 23:00 uur bereikbaar zijn via onze sociale kanalen?
+        
+          </p>
+
+          <div class="info">
+            <div class="information">
+              <img src="img/location.png" class="icon" alt="" />
+              <p>Grauwaartsingel 260, 3541 DA Utrecht</p>
+            </div>
+            <div class="information">
+              <img src="img/email.png" class="icon" alt="" />
+              <p>mboutrecht@outlook.nl</p>
+            </div>
+            <div class="information">
+              <img src="img/phone.png" class="icon" alt="" />
+              <p>030 666 7737</p>
+            </div>
+          </div>
+
+          <div class="social-media">
+            <p>Connect with us :</p>
+            <div class="social-icons">
+              <a href="https://nl-nl.facebook.com/" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="https://twitter.com/"  target="_blank">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="https://www.instagram.com/"  target="_blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://nl.linkedin.com/"  target="_blank">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+       
+  </body>
+  
+  <footer> made by abderrahman & mohammed</footer>     
+        
+</html>
