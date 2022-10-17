591fa451635eb2f9209844016725f807e9f61c7a
MohammedChebab <333474@student.mboutrecht.nl>
2022-10-17 10:47:12 +0200

    feat: ik heb alles weg gehaald en telefoonnummer, email en adres toegevoegd

--- navigatie.php
+++ navigatie.php
@@ -39,43 +39,13 @@
 </div> -->
 
 
-<div class="row">
-      <div class="column" style="background-color:#3c4053">
-        <div class="container">
-        <span>Locatie</span>
-        <h3>ICT Academie</h3>
-        <p>
-            Daltonlaan 300 <br>
-            Utrecht </p>
-
-        <p>
-         <a href="mailto:studiekeuze@mboutrecht.nl">studiekeuze@mboutrecht.nl</a><br>
-         <a href="tel:"></a>
-        </p>
-
-        <p>
-         <a href="whatsapp://send?phone=31625705051" target="_blank">+31 (0) 6 25705051</a> <span
-         class="small">(WhatsApp)</span>
-        </p>
-    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9806.
-    358403289614!2d5.151535498693468!3d52.08720037733886!2m3!1f0!2f0!3f0!3m2
-    !1i1024!2i768!4f13.1!3m3!1m2!1s0x47c668a291ed7f45%3A0x9b18dabb8687ad2!2
-    sMBO%20Utrecht!5e0!3m2!1snl!2snl!4v1663069808762!5m2!1snl!2snl" width="900" height="500" style="border:0;"
-        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
-
-
-
-          
-        
-          </div>
-           <!-- <h1>560315</h1>  -->
-      </div>
-     
-        
-        
-        
-        </div>
-    
-      </div>
-    </div>
-
+<div class="contact-in">
+            <h1>Contact Us</h1>
+            <h2><i class="fa fa-phone" aria-hidden="true"></i> Telefoon</h2>
+            <p>+31 (0) 6 25705051</p>
+            <h2><i class="fa fa-envelope" aria-hidden="true"></i> Email</h2>
+            <p>info@mboutrecht.nl</p>
+            <h2><i class="fa fa-marker" aria-hidden="true"></i> Address</h2>
+            <p>Daltonlaan 300
+                Utrecht</p>
+</div>
