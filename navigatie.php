049ede8917d69b00bd9bccb998bd44c946c389dc
MohammedChebab <333474@student.mboutrecht.nl>
2022-10-17 11:16:33 +0200

    feat:ik heb de google maps op ons website gedaan waar je de locatie van ons school ziet

--- navigatie.php
+++ navigatie.php
@@ -60,6 +60,10 @@
             </ul>
      </div>
      <div class="contact-in">
+     <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.5931112709604!2d5.158462715679926!3d52.08713647973455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c668a2918b16e9%3A0x95fac93d2c242c97!2sDaltonlaan%20300%2C%203584%20BK%20Utrecht!5e0!3m2!1snl!2snl!4v1665944679665!5m2!1snl!2snl"
+                 width="600" height="auto" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
      </div>
      </div>
      
